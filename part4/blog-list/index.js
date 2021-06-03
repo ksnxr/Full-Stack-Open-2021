5cd15086b2b02b84affcd8a119a575c244eeb2aa
ksnxr <random_username_1@outlook.com>
2021-06-04 01:29:26 +0800

    Finish 4.1 and part of 4.2.

---
+++ part4/blog-list/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = config.MONGODB_URL
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog.save().then((result) => {
+        response.status(201).json(result)
+    })
+})
+
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+})
